dbd7f97aa6e4d28cebce684b73e72d5a59c64392
Juan <juans@pinterest.com>
2025-01-15 16:07:01 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello Zealous, hope we work together!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});
